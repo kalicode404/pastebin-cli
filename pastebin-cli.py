8a6a8e804f4cc4f5b9f820515d7819ea91675195
Unknown <kalicode404@gmail.com>
2018-04-13 03:27:17 +0800

    fix typo

--- pastebin-cli.py
+++ pastebin-cli.py
@@ -1,7 +1,11 @@
-<<<<<<< Updated upstream
-=======
+
 import urllib
-print (' #########   C0D3404   ######### \n\n')
+print"##############################"
+print"#        : C0D3404 :         #"
+print"#  If you want to make this  #"
+print"#     script more awesome    #"
+print"#     please contribute      #"
+print"##############################"
 
 pastebin_vars = {'api_dev_key':raw_input('Your API KEY ==>>'),'api_option':'paste','api_paste_code':raw_input("Paste what you want to paste ===>>\n")}
 response = urllib.urlopen('http://pastebin.com/api/api_post.php', urllib.urlencode(pastebin_vars))
