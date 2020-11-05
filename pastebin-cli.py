51c6e9abc3825a74c774c0b80496f42fc5aa8c7a
C0D34O4 <kalicode404@gmail.com>
2020-11-05 04:35:09 -0800

    update
    add 2020 text

--- pastebin-cli.py
+++ pastebin-cli.py
@@ -4,7 +4,7 @@ print"##############################"
 print"#        : C0D3404 :         #"
 print"#  If you want to make this  #"
 print"#     script more awesome    #"
-print"#     please contribute      #"
+print"#     please contribute 2020 #"
 print"##############################"
 
 pastebin_vars = {'api_dev_key':raw_input('Your API KEY ==>>'),'api_option':'paste','api_paste_code':raw_input("Paste what you want to paste ===>>\n")}
